8c28080bd5c6c0fd27b592a0a75bb12d2cc809f8
Kysohee18 <hanashahky@gmail.com>
2026-04-14 09:45:54 +0700

    setup library for User controller

--- src/main/java/com/tugas/deploy/Controller/UserController.java
+++ src/main/java/com/tugas/deploy/Controller/UserController.java
@@ -1,4 +1,11 @@
 package com.tugas.deploy.Controller;
+import com.tugas.deploy.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.;
 
+import java.util.ArrayList;
+import java.util.List;
+@Controller
 public class UserController {
 }
