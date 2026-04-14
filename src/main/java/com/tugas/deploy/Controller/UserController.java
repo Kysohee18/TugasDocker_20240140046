7303b4f09edacad3918ee890551970001efc4e4f
Kysohee18 <hanashahky@gmail.com>
2026-04-14 09:51:39 +0700

    add login html

--- src/main/java/com/tugas/deploy/Controller/UserController.java
+++ src/main/java/com/tugas/deploy/Controller/UserController.java
@@ -11,20 +11,19 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    // List statis untuk menyimpan data sementara selama aplikasi berjalan
+    //untuk menyimpan data sementara selama aplikasi berjalan
     private static List<User> listMahasiswa = new ArrayList<>();
 
     @GetMapping("/")
     public String loginPage() {
-        return "login"; // Mengarah ke login.html [cite: 56]
+        return "login";
     }
 
     @PostMapping("/login")
     public String prosesLogin(@RequestParam String username,
                               @RequestParam String password,
                               Model model) {
-        // Logika autentikasi sesuai requirement [cite: 70]
-        // Username: admin, Password: NIM Anda (20240140046)
+
         if ("admin".equals(username) && "20240140046".equals(password)) {
             return "redirect:/home";
         }
@@ -35,18 +34,18 @@ public class UserController {
     @GetMapping("/home")
     public String homePage(Model model) {
         model.addAttribute("dataMahasiswa", listMahasiswa);
-        return "home"; // Mengarah ke home.html [cite: 55]
+        return "home";
     }
 
     @GetMapping("/form")
     public String formPage(Model model) {
         model.addAttribute("user", new User());
-        return "form"; // Mengarah ke form.html [cite: 54]
+        return "form";
     }
 
     @PostMapping("/save")
     public String simpanData(@ModelAttribute User user) {
-        listMahasiswa.add(user); // Data disimpan secara temporary
+        listMahasiswa.add(user);
         return "redirect:/home";
     }
 
